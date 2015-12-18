aaae5f52cdbce163d834abb09c31b398d9db4da4
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2015-12-18 07:46:17 -0500

    User Story 6 completed

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'faker'
 gem 'pundit'
 gem 'devise'
 gem 'bootstrap-sass'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+include Faker
+
+30.times do
+  Wiki.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+15.times do
+  User.create!(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.safe_email,
+    password: Faker::Internet.password(8)
+  )
+end
+
+
+puts "Seed finished"
+puts "#{Wiki.count} wikis created"
+puts "#{User.count} users created"
