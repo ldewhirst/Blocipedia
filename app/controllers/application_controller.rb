33ac494349a723dbb4b88d1d4865d4e862bfd0af
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2015-12-18 09:28:06 -0500

    User Story 6 edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
@@ -15,5 +17,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  end
+
+private
+  def user_not_authorized
+    flash[:error] = "You are not authorized for this action"
+    redirect_to(request.referrer || root_path)
   end
 end
--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -47,6 +47,7 @@ class WikisController < ApplicationController
   def destroy
     @wiki = Wiki.find(params[:id])
 
+    authorize @wiki
     if @wiki.destroy
       flash[:notice] = "\"#{@wiki.title}\" was deleted successfully"
       redirect_to wikis_path
