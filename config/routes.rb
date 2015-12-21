d3d8dd6971e1f6c25cee334c428179e3d8639741
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2015-12-21 06:45:01 -0500

    User Story 7 edits

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   resources :charges, only: [:new, :create]
 
+  patch '/charges/downgrade' => 'charges#downgrade', as: :downgrade
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
