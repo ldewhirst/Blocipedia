cbc29f48abe4982c773d4b0f4aee7f43b59258a4
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2016-01-03 15:23:10 -0500

    Collaborations

--- db/schema.rb
+++ db/schema.rb
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20160102135807) do
     t.integer  "user_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.text     "markdown_content"
   end
 
   add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
