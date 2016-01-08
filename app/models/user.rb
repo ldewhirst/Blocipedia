01fcf8d9cfc07b0ff720e35a506eea985eae18c4
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2016-01-08 07:51:06 -0500

    Navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+
   has_many :wikis
   has_many :collaborations
   has_many :wiki_collaborations, through: :collaborators, source: :wiki
 
-  
+
   after_initialize :init
 
   def init
