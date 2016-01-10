a29a904304225ef0b0641f45bf7428f3a704abc2
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2016-01-10 11:59:19 -0500

    Navbar Updates

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   patch '/charges/downgrade' => 'charges#downgrade', as: :downgrade
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
